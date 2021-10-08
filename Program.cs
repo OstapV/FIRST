c55d46aa63af6757625ffdb91266dbc5bfc99c2c
OstapV <92176833+OstapV@users.noreply.github.com>
2021-10-08 20:21:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace SIGMA_5_FIRST_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "c:\\WebServers\\home\\testsite\\www\\myfile.txt";
+            var arr = path.Split("\\");
+
+            string result = Path.GetFileNameWithoutExtension(path);
+            Console.WriteLine("File name without extension: " + result);
+
+            string root = Path.GetPathRoot(path);
+            Console.WriteLine("Path root: " + root);
+
+            int fileIndex = arr.GetUpperBound(0);
+            var nameWithoutExt = arr[fileIndex].Split('.');
+            Console.WriteLine("Name without extension: " + nameWithoutExt[0]);
+            
+            Console.WriteLine("Path root: " + arr[0]);
+
+        }
+    }
+}
